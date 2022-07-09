4b3bd32dd82ab9f70463ea975cc69d60f962fa51
Nishitha345 <108972792+Nishitha345@users.noreply.github.com>
2022-07-09 14:21:27 +0530

    Create day_1_prob_3.c

---
+++ DAY1/day_1_prob_3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int r,a;
+    printf("the radius of the garden");
+    scanf("%d",&r);                              //Read the input from the user
+    a=3.14*r*r;                                  //calulate the area using formula
+    printf(" area of garden is %d ",a);          
+}
