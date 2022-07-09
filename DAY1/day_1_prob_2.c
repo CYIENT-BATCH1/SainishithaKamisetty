1a96eb48b55e27cac4bb65770cbc42866cb4c41e
Nishitha345 <108972792+Nishitha345@users.noreply.github.com>
2022-07-09 14:12:53 +0530

    Create day_1_prob_2.c

---
+++ DAY1/day_1_prob_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+char i;
+
+printf("enter the character\n");
+scanf("%c",&i);                            //Read the input from the user
+if(i=='A'||i=='E'||i=='I'||i=='O'||i=='U'||i=='a'||i=='e'||i=='i'||i=='o'||i=='u')   //if the condition true then print vowels
+printf("vowels");
+
+else
+printf("consonants");
+    return 0;
+}
