ef78056868664b3504d74a3c9f2237fe72104b8c
Nishitha345 <108972792+Nishitha345@users.noreply.github.com>
2022-07-10 19:34:16 +0530

    Create day_11_Alog_1.c

---
+++ DAY11/day_11_Alog_1.c
@@ -0,0 +1,12 @@
+1. Write a program to do binary search in a sorted array. Program should take input of sorted values and search value. 
+(Make number of elements as configurable value)
+How binary search works is explained below.
+  
+ALOGRITHM
+1)Instalize the variables
+2)Read the inputs
+3)set low= starting position(x) and high =ending position(n-1)
+4)set the mid=(low+high)/2 to find mid term
+5)If array of mid is lessthan means low=mid+1 it will search for left half
+6)If array of mid is greaterthan means high=mid-1 it will search for right half
+7)if the result is zero print element is not found.
